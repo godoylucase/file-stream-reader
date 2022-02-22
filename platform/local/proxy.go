31e42f49070f31f9aa57e263b7a36e8b10a60755
Lucas Godoy <lucas.godoy@pomelo.la>
2022-02-22 09:17:40 -0300

    Local file proxy (#11)

---
+++ platform/local/proxy.go
@@ -0,0 +1,42 @@
+package local
+
+import (
+	"os"
+)
+
+type localProxy struct {
+}
+
+func NewProxy() *localProxy {
+	return &localProxy{}
+}
+
+func (l *localProxy) Length(filename string) (int64, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	stat, err := file.Stat()
+	if err != nil {
+		return 0, err
+	}
+
+	return stat.Size(), nil
+}
+
+func (l *localProxy) Bytes(filename string, from int64, to int64, chunk []byte) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.ReadAt(chunk, from)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
